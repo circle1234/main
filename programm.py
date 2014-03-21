92825408317eecf97f7de6526a1b47f00a209628
circle1234 <circle1234@live.ru>
2014-03-21 16:46:50 -0700

    Update programm.py

--- programm.py
+++ programm.py
@@ -1,1 +1,20 @@
 """dasdas"""
+import string                       
+from urllib2 import urlopen         
+def top_words(s):
+    words = {}                                                             
+    for line in s.readlines():          
+        line = string.strip(line, " \n")    
+        for word in line.split(" "): 
+            try:                     
+                words[word] += 1            
+            except KeyError:                
+                words[word] = 1             
+ 
+            pairs = words.items()               
+            pairs.sort(lambda a, b: b[1]-a[1])   
+            for p in pairs[:10]:                
+                print p[0], p[1]
+web=raw_input("Enter a web address:")
+u = urlopen(web)
+top_words(u)
